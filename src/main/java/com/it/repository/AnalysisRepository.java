aed36d0fd1a27735f4458647d58cdefd55c6f8c5
chenxiujiang <chenxiujiang@isec-tech.com>
2018-05-23 12:51:42 +0800

    重构

--- src/main/java/com/it/repository/AnalysisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.it.repository;
-
-import com.it.bean.AnalysisStock;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AnalysisRepository extends MongoRepository<AnalysisStock, Integer> {
-
-}
